295734bae0d7bd7e897d2c66f01e2ab62db41cea
Julien Nioche <julien@digitalpebble.com>
2025-06-18 10:57:58 +0100

    Removed unused code for reading from JSON resource files
    Signed-off-by: Julien Nioche <julien@digitalpebble.com>

--- src/main/java/com/digitalpebble/carbonara/modules/ccf/Networking.java
+++ src/main/java/com/digitalpebble/carbonara/modules/ccf/Networking.java
@@ -2,9 +2,9 @@
  * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.  The ASF licenses this file to you under the Apache License, Version
  * 2.0 (the "License"); you may not use this file except in compliance with the License.  You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
  * and limitations under the License.
@@ -14,13 +14,7 @@ package com.digitalpebble.carbonara.modules.ccf;
 
 import com.digitalpebble.carbonara.Column;
 import com.digitalpebble.carbonara.EnrichmentModule;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.spark.sql.Row;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Map;
 
 import static com.digitalpebble.carbonara.Column.ENERGY_USED;
 
@@ -38,21 +32,6 @@ public class Networking implements EnrichmentModule {
     @Override
     public Column[] columnsAdded() {
         return new Column[]{ENERGY_USED};
-    }
-
-    public static Map<String, Object> loadResources(String resourceFileName) throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        try (InputStream inputStream = Networking.class
-                .getClassLoader()
-                .getResourceAsStream(resourceFileName)) {
-
-            if (inputStream == null) {
-                throw new IOException("Resource file not found: " + resourceFileName);
-            }
-
-            return objectMapper.readValue(inputStream, new TypeReference<Map<String, Object>>() {});
-        }
     }
 
     @Override
