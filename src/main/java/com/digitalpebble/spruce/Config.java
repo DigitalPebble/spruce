2586b781d32866833e127ad8da741ae7a8eab1da
Julien Nioche <julien@digitalpebble.com>
2025-07-31 14:33:49 +0100

    Parse CLI arguments with lib and load default config if not set explicitly. Fixes #13
    Signed-off-by: Julien Nioche <julien@digitalpebble.com>

--- src/main/java/com/digitalpebble/spruce/Config.java
+++ src/main/java/com/digitalpebble/spruce/Config.java
@@ -2,8 +2,12 @@
 
 package com.digitalpebble.spruce;
 
+import com.digitalpebble.spruce.modules.ccf.Networking;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
 
@@ -29,18 +33,39 @@ public class Config {
         }
     }
 
+    public static Config loadDefault() throws java.io.IOException  {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        final String resourceFileName = "default-config.json";
+        try (InputStream inputStream = Config.class
+                .getClassLoader()
+                .getResourceAsStream(resourceFileName)) {
+
+            if (inputStream == null) {
+                throw new IOException("Resource file not found: " + resourceFileName);
+            }
+
+            Map<String, Object> startNode = objectMapper.readValue(inputStream, new TypeReference<Map<String, Object>>() {});
+            return process(startNode);
+        }
+    }
+
+
+
     /**
      * Loads a Config instance from a JSON file
      */
     public static Config fromJsonFile(java.nio.file.Path path) throws java.io.IOException {
         com.fasterxml.jackson.databind.ObjectMapper mapper = new com.fasterxml.jackson.databind.ObjectMapper();
-        // Read the JSON as a List of Maps
         Map<String, Object> startNode = mapper.readValue(
                 java.nio.file.Files.newBufferedReader(path),
                 new TypeReference<Map<String, Object>>() {
                 }
         );
+        return process(startNode);
+    }
 
+    private static Config process(Map<String, Object> startNode){
         List<Map<String, Object>> modulesList = (List<Map<String, Object>>) startNode.get("modules");
 
         Config conf = new Config();
