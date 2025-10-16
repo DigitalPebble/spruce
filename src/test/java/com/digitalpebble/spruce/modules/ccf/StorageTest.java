55b2724417abeed1699d89932dead4676edbc2b8
Julien Nioche <julien@digitalpebble.com>
2025-10-16 20:58:41 +0100

    Add replication factors for storage, fix bug #65
    Signed-off-by: Julien Nioche <julien@digitalpebble.com>

--- src/test/java/com/digitalpebble/spruce/modules/ccf/StorageTest.java
+++ src/test/java/com/digitalpebble/spruce/modules/ccf/StorageTest.java
@@ -8,8 +8,12 @@ import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema;
 import org.apache.spark.sql.types.StructType;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static com.digitalpebble.spruce.SpruceColumn.ENERGY_USED;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -27,28 +31,28 @@ class StorageTest {
 
     @Test
     void processCreateVolumeSSD() {
-        Object[] values = new Object[]{"CreateVolume-Gp3", 10d, null, null, null, null};
+        Object[] values = new Object[]{"CreateVolume-Gp3", 10d, "VolumeUsage.gp3", "AmazonEC2", "GB-Mo", null};
         Row row = new GenericRowWithSchema(values, schema);
         Row result = storage.process(row);
-        double expected = 10d * storage.ssd_gb_coefficient;
+        double expected = 10d * storage.ssd_gb_coefficient * 2;
         assertEquals(expected, ENERGY_USED.getDouble(result));
     }
 
     @Test
     void processCreateVolumeSSD2() {
-        Object[] values = new Object[]{"CreateVolume-Gp2", 10d, null, null, null, null};
+        Object[] values = new Object[]{"CreateVolume-Gp2", 10d, "EBS:VolumeUsage.gp2", "AmazonEC2", "GB-Mo", null};
         Row row = new GenericRowWithSchema(values, schema);
         Row result = storage.process(row);
-        double expected = 10d * storage.ssd_gb_coefficient;
+        double expected = 10d * storage.ssd_gb_coefficient * 2;
         assertEquals(expected, ENERGY_USED.getDouble(result));
     }
 
     @Test
     void processCreateVolumeHDD() {
-        Object[] values = new Object[]{"CreateVolume", 10d, null, null, null, null};
+        Object[] values = new Object[]{"CreateVolume", 10d, "EUW2-EBS:VolumeUsage", "AmazonEC2", "GB-Mo", null};
         Row row = new GenericRowWithSchema(values, schema);
         Row result = storage.process(row);
-        double expected = 10d * storage.hdd_gb_coefficient;
+        double expected = 10d * storage.hdd_gb_coefficient * 2;
         assertEquals(expected, ENERGY_USED.getDouble(result));
     }
 
@@ -66,16 +70,26 @@ class StorageTest {
         Object[] values = new Object[]{"Storage", 10d, "SomeUsageType", "AmazonDocDB", "GB-Mo", null};
         Row row = new GenericRowWithSchema(values, schema);
         Row result = storage.process(row);
-        double expected = 10d * storage.ssd_gb_coefficient;
+        double expected = 10d * storage.ssd_gb_coefficient * 2;
         assertEquals(expected, ENERGY_USED.getDouble(result));
     }
 
-    @Test
-    void processSSDServiceWrongUInit() {
-        Object[] values = new Object[]{"Storage", 10d, "SomeUsageType", "AmazonDocDB", "vCPU-hour", null};
+    @ParameterizedTest
+    @MethodSource("provideArgsWrongUnit")
+    void processSSDServiceWrongUnit(String LINE_ITEM_OPERATION, double USAGE_AMOUNT, String LINE_ITEM_USAGE_TYPE, String PRODUCT_SERVICE_CODE, String PRICING_UNIT) {
+        Object[] values = new Object[]{LINE_ITEM_OPERATION, USAGE_AMOUNT, LINE_ITEM_USAGE_TYPE, PRODUCT_SERVICE_CODE, PRICING_UNIT, null};
         Row row = new GenericRowWithSchema(values, schema);
         Row result = storage.process(row);
         assertEquals(row, result);
+    }
+
+
+    private static Stream<Arguments> provideArgsWrongUnit() {
+        return Stream.of(
+                Arguments.of("Storage", 10d, "SomeUsageType", "AmazonDocDB", "vCPU-hour"),
+                Arguments.of("CreateVolume-Gp3", 0.1, "EBS:VolumeP-IOPS.gp3", "AmazonEC2", "IOPS-Mo"),
+                Arguments.of("CreateVolume-Gp2", 0.1, "EBS:VolumeP-Throughput.gp3", "AmazonEC2", "GiBps-mo")
+        );
     }
 
 }
