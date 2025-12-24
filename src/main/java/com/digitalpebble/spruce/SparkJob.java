29dd333d333158904010d6cf1a7d851c4e556d1b
Julien Nioche <julien@digitalpebble.com>
2025-12-24 10:14:42 +0000

    Spark always runs in local mode, fixes #122
    Signed-off-by: Julien Nioche <julien@digitalpebble.com>

--- src/main/java/com/digitalpebble/spruce/SparkJob.java
+++ src/main/java/com/digitalpebble/spruce/SparkJob.java
@@ -41,7 +41,7 @@ public class SparkJob {
 
         SparkSession spark = SparkSession.builder()
                 .appName("SPRUCE")
-                .master("local[*]")
+        //        .master("local[*]")
                 .getOrCreate();
 
         spark.conf().set("mapreduce.fileoutputcommitter.marksuccessfuljobs", "false");
